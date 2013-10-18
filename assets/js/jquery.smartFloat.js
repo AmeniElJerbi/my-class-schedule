25c10105bbbece80451d04fa73bf56b597bd7658
元兒～ <yuan817@moztw.org>
2013-10-19 02:36:37 +0800

    Data Update

--- assets/js/jquery.smartFloat.js
+++ assets/js/jquery.smartFloat.js
@@ -1,4 +1,4 @@
-//來源: http://www.helloweba.com/view-blog-194.html
+//來源: http://www.zhangxinxu.com/wordpress/2010/11/js%E9%A1%B5%E9%9D%A2%E6%BB%9A%E5%8A%A8%E6%97%B6%E5%B1%82%E6%99%BA%E8%83%BD%E6%B5%AE%E5%8A%A8%E5%AE%9A%E4%BD%8D%E5%AE%9E%E7%8E%B0jquerymootools/
 
 $.fn.smartFloat = function() {
     var position = function(element) {
